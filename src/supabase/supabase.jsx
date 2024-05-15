7d6b80e36503fea7511dbe5d7dcf970f50f4813b
LeonDev-0 <zandrotja@gmail.com>
2024-05-15 18:06:53 -0400

    cambio de apikey

--- src/supabase/supabase.jsx
+++ src/supabase/supabase.jsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const url='https://hrxffgxmtjdrwomttuxl.supabase.co'
-const key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyeGZmZ3htdGpkcndvbXR0dXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0ODcwNjYsImV4cCI6MjAyOTA2MzA2Nn0.mI8B3_gyRKfsIOMa-ZFDT_sejIowzx2qi_HMiodqPJY'
+const url='https://ppjycaopepgezicyvpgm.supabase.co'
+const key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwanljYW9wZXBnZXppY3l2cGdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MDczOTgsImV4cCI6MjAzMTM4MzM5OH0.e447N1jnzKsRRZDtBg1D9Sh-PoTI1UqetGn4I2EGFmI'
 export const supabase = createClient(url,key
 );
